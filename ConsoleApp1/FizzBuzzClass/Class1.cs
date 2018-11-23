2fd8f33f97024a59a4b80f93264b5b2a3d3a3a94
nilstuetje <nilstuetje94@gmail.com>
2018-11-23 20:30:18 +0100

    initial commit

---
+++ ConsoleApp1/FizzBuzzClass/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FizzBuzzClass
+{
+    /// <summary>
+    /// class to hold methodes for FizzBuzz, a simple programming challange that prints out a number of items with all dividable by 3 beeing
+    /// replaced by Fizz, dividable by 5 Buzz and dividable by both FizzBuzz
+    /// </summary>
+    public static class FizzBuzz
+    {
+        //variabbles to hold the start and end point of the fizzbuzz sequence, defaulting to 0 to 100
+        static int start = 0;
+        static int end = 100;
+
+        //variables holding the divders to replace by a word
+        static int fizz = 3;
+        static int buzz = 5;
+        static int fizzbuzz = 3 * 5;   
+
+        
+        public static int[] Calculate(int start, int end)
+        {
+
+            return null;
+        }
+
+
+    }
+}
